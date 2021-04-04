e44792d6509adef296383a0c0c7b58b357d2176a
kakou34 <kaouther.mouheb@gmail.com>
2021-04-04 23:21:53 +0300

    Back-end done

---
+++ client.py
@@ -0,0 +1,69 @@
+import time
+from socket import *
+
+
+host = str(input("Enter server address: "))
+port = 1234
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((host, port))
+
+print('Waiting for the connection...')
+
+client_name = input('Enter your name:')
+client_socket.send(client_name.encode())
+server_name = client_socket.recv(1024)
+server_name = server_name.decode()
+print(server_name, 'is ready!')
+
+words = []
+
+server_message = client_socket.recv(1024)
+server_message = server_message.decode()
+words.append(server_message)
+print((server_name, server_message))
+
+time_left = 20
+while True:
+
+    start_time = time.time()
+    client_message = input("Enter your word:")
+
+    if client_message == 'exit':
+        message = client_name + " left the game!"
+        print(message)
+        client_socket.send(message.encode())
+        break
+
+    elif server_message[-2:] != client_message[0:2]:
+        print("The word must start with ", server_message[-2:])
+        time_spent = time.time() - start_time
+        time_left = time_left - time_spent
+        continue
+
+    elif client_message in words:
+        print("The word you entered has already been used, try again!")
+        time_spent = time.time() - start_time
+        time_left = time_left - time_spent
+        continue
+
+    else:
+        time_spent = time.time() - start_time
+        time_left = time_left - time_spent
+        if time_left <= 0:
+            print("time is up", server_name, "won!")
+            message = "Time is up! You won!"
+            client_socket.send(message.encode())
+            break
+        client_socket.send(client_message.encode())
+        words.append(client_message)
+
+    server_message = client_socket.recv(1024)
+    server_message = server_message.decode()
+    exit_msg = server_name + " left the game!"
+    if (server_message == 'Time is up! You won!') or (server_message == exit_msg):
+        print(server_message)
+        break
+    words.append(server_message)
+    print((server_name, server_message))
+    time_left = 20
---
+++ server.py
@@ -0,0 +1,87 @@
+import socket
+import time
+import sys
+
+print('Welcome to the game!')
+
+port = 1234
+host = ''
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(socket.gethostname())
+server_socket.bind((socket.gethostname(), port))
+server_socket.listen(2)
+connection, add = server_socket.accept()
+
+server_name = input('Please enter your name:')
+
+client_name = connection.recv(1024)
+client_name = client_name.decode()
+print(client_name, "is ready!")
+connection.send(server_name.encode())
+
+words = []
+
+while True:
+    server_message = input('Enter the first word:')
+    if len(server_message)>1:
+        connection.send(server_message.encode())
+        words.append(server_message)
+        break
+    print("Your word should contain at least 2 characters.")
+
+client_message = connection.recv(1024)
+client_message = client_message.decode()
+exit_msg = client_name + " left the game!"
+if (client_message == 'Time is up! You won!') or (client_message == exit_msg):
+    print(client_message)
+    sys.exit()
+words.append(client_message)
+print(client_name, client_message)
+
+time_left = 20
+while True:
+
+    start_time = time.time()
+    server_message = input('Enter your word:')
+
+    if server_message == 'exit':
+        message = server_name + " left the game!"
+        print(message)
+        connection.send(message.encode())
+        break
+    elif client_message[-2:] != server_message[0:2]:
+        print("The word must start with ", client_message[-2:])
+        time_spent = time.time() - start_time
+        time_left = time_left - time_spent
+        continue
+    elif server_message in words:
+        print("The word you entered has already been used, try again!")
+        time_spent = time.time() - start_time
+        time_left = time_left - time_spent
+        continue
+    else:
+        time_spent = time.time() - start_time
+        time_left = time_left - time_spent
+        if time_left <= 0:
+            print("time is up", client_name, "won!")
+            message = "Time is up! You won!"
+            connection.send(message.encode())
+            break
+        connection.send(server_message.encode())
+        words.append(server_message)
+
+    client_message = connection.recv(1024)
+    client_message = client_message.decode()
+    exit_msg = client_name + " left the game!"
+    if (client_message == 'Time is up! You won!') or (client_message == exit_msg):
+        print(client_message)
+        break
+    words.append(client_message)
+    print(client_name, client_message)
+
+    time_left = 20
+
+
+
+
+
